77138ccfabb9e339334d833cc36fd6aa58a10e95
tanishkajain22 <49120994+tanishkajain22@users.noreply.github.com>
2020-11-01 00:24:35 +0530

    Update PrintAllPermutations.cpp

--- Permutation/PrintAllPermutations.cpp
+++ Permutation/PrintAllPermutations.cpp
@@ -1,27 +1,36 @@
-#include <iostream>
-using namespace std;
+# Python function to print permutations of a given list 
+def permutation(lst): 
 
-void printPermutations(string s, int i){
+	# If lst is empty then there are no permutations 
+	if len(lst) == 0: 
+		return [] 
 
-    if (i==s.size()- 1){
-        cout<<s<<"\n";
-        return ;
-    }
+	# If there is only one element in lst then, only 
+	# one permuatation is possible 
+	if len(lst) == 1: 
+		return [lst] 
 
-    char c = s[i];
-    for(int j=i; j<s.size(); j++){
-        s[i] = s[j];
-        s[j] = c;
-        printPermutations(s, i+1);
-        s[j] = s[i];
-        s[i] = c;
-    }
+	# Find the permutations for lst if there are 
+	# more than 1 characters 
 
-}
+	l = [] # empty list that will store current permutation 
 
-int main(){
-    string s;
-    cin>>s;
+	# Iterate the input(lst) and calculate the permutation 
+	for i in range(len(lst)): 
+	m = lst[i] 
 
-    printPermutations(s, 0);
-}
+	# Extract lst[i] or m from the list. remLst is 
+	# remaining list 
+	remLst = lst[:i] + lst[i+1:] 
+
+	# Generating all permutations where m is first 
+	# element 
+	for p in permutation(remLst): 
+		l.append([m] + p) 
+	return l 
+
+
+# Driver program to test above function 
+data = list('123') 
+for p in permutation(data): 
+	print p 
